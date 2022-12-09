be9d13545f0082be3d0c3141b99bbc26e10341c5
pinki-mandal <99741856+pinki-mandal@users.noreply.github.com>
2022-12-09 23:15:20 +0530

    Add files via upload

---
+++ presentation.js
@@ -0,0 +1,6 @@
+// alert:-The alert() method in JavaScript displays an alert box with a specified message and an OK button.
+
+// src:-The src attribute specifies the URL of an external script file. 
+
+
+// heap or stock
